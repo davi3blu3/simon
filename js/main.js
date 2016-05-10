e04c80cf1d876795d7d2f159181fa0db131cf8b8
Dave Harned <dharned@gmail.com>
2016-05-09 23:00:39 -0500

    update colors

--- js/main.js
+++ js/main.js
@@ -1,38 +1,38 @@
 $(document).ready(function() {
 
-	function changeColor(el, color) {
+	function revertColor(el, color) {
 		$(el).css("background-color", color);
 	}
 
 	$(".top-left").on("click", function() {
 		var original = $(this).css("background-color");
-		$(this).css("background-color", "black");
+		$(this).css("background-color", "green");
 		setTimeout(function(el, color) {
-			changeColor(el, color);
+			revertColor(el, color);
 		}, 400, this, original);
 	})
 
 	$(".top-right").on("click", function() {
 		var original = $(this).css("background-color");
-		$(this).css("background-color", "black");
+		$(this).css("background-color", "red");
 		setTimeout(function(el, color) {
-			changeColor(el, color);
+			revertColor(el, color);
 		}, 400, this, original);
 	})
 
 	$(".btm-left").on("click", function() {
 		var original = $(this).css("background-color");
-		$(this).css("background-color", "black");
+		$(this).css("background-color", "yellow");
 		setTimeout(function(el, color) {
-			changeColor(el, color);
+			revertColor(el, color);
 		}, 400, this, original);
 	})
 
 	$(".btm-right").on("click", function() {
 		var original = $(this).css("background-color");
-		$(this).css("background-color", "black");
+		$(this).css("background-color", "blue");
 		setTimeout(function(el, color) {
-			changeColor(el, color);
+			revertColor(el, color);
 		}, 400, this, original);
 	})			
 
