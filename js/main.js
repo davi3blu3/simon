522341f980c7d9031beed447445370b982c2b1e9
Dave Harned <dharned@gmail.com>
2016-06-28 13:02:02 -0500

    start game flow

--- js/main.js
+++ js/main.js
@@ -9,23 +9,44 @@ $(document).ready(function() {
 		}, 500, element, original);
     }
     
+    function playPattern(num) {
+        switch (num) {
+            case 0:
+                colorChange($(".green"), "lime");
+                break;
+            case 1:
+                colorChange($(".red"), "red");
+                break;
+            case 2:
+                colorChange($(".yellow"), "yellow");
+                break;
+            case 3:
+                colorChange($(".blue"), "blue");
+                break;
+        }
+    }
+    
     
     // CLICK EVENTS
     
 	$(".green").on("click", function() {
         colorChange(this, "lime");
+        current.userPattern.push(0);
 	})
 
 	$(".red").on("click", function() {
         colorChange(this, "red");
+        current.userPattern.push(1);
 	})
 
 	$(".yellow").on("click", function() {
         colorChange(this, "yellow");
+        current.userPattern.push(2);
 	})
 
 	$(".blue").on("click", function() {
         colorChange(this, "blue");
+        current.userPattern.push(3);
 	})
     
     // GAME OBJECT
@@ -41,17 +62,39 @@ $(document).ready(function() {
             return random;
         }
         
-        // simon's turn triggering animation
-        this.turn = function() {
-            this.simonPattern.push(this.randomInt());
-            var testPattern = [1, 3, 0, 2, 1, 1, 2, 3, 0];
-            
-            // return this.simonPattern;
-        }
+
     }
 
-    var current = new Simon();
+    
     // console.log(current.turn());
+    
+    // PSEUDO CODE GAME FLOW
+    // Create new game object
+    var current = new Simon(),
+        i;
+    
+    // Generate one random number, add to simonPattern
+    current.simonPattern.push(current.randomInt());
+    current.simonPattern.push(current.randomInt());
+    current.simonPattern.push(current.randomInt());
+    current.simonPattern.push(current.randomInt());
+    current.simonPattern.push(current.randomInt());
+    current.simonPattern.push(current.randomInt());
+    current.simonPattern.push(current.randomInt());
+    
+    // Trigger timed color changes according to simonPattern
+//    current.simonPattern.forEach(function(number) {
+//        setTimeout(function(number) {
+//			playPattern(number);
+//		}, 2000, number);
+//    });
+    
+    current.simonPattern.forEach(function(number, index) {
+        setTimeout(playPattern.bind(null, number), index*1000);
+    });
+    // Wait for user input
+    // User clicks checked against simonPattern, pushed to userPatter
+    
 
 
 })
