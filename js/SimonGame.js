0fe70897d81610947f5a0d4d93fc38808b7b1fa4
Dave Harned <dharned@gmail.com>
2016-10-11 23:44:12 -0500

    start refactor of javascript

---
+++ js/SimonGame.js
@@ -0,0 +1,20 @@
+var SimonGame = function() {
+
+    var computerSequence = [];
+    var playerSequence = [];
+    var strictMode = false;
+
+    return {
+        resetGame: function() {
+            computerSequence = [];
+            playerSequence = [];
+        },
+        computersTurn: function(turnNumber) {
+
+        },
+        playersInput: function(buttonPressed) {
+
+        }
+    };
+
+}(SimonGame || {});
