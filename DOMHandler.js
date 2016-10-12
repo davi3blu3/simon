84d0a16c3fa4cd519eb0a6bf5d5cb2756268e5da
Dave Harned <dharned@gmail.com>
2016-10-11 23:58:14 -0500

    add DOM Handler and connect to DOM elements

---
+++ DOMHandler.js
@@ -0,0 +1,10 @@
+// references to game buttons
+var greenBtn = document.getElementById("green");
+var redBtn = document.getElementById("red");
+var yellowBtn = document.getElementById("yellow");
+var blueBtn = document.getElementById("blue");
+
+// references to game controls
+var roundCounter = document.getElementById("counter");
+var resetBtn = document.getElementById("reset");
+var strictBtn = document.getElementById("strict");
---
+++ buttonPlay.js
@@ -0,0 +1,3 @@
+var SimonGame = function(game) {
+
+}(SimonGame || {});
