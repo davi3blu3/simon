45dcc3cad2d37ccd5e890371a8a814de6daa7db4
Dave Harned <dharned@gmail.com>
2016-12-18 23:51:46 -0600

    testing css element manipulation

--- js/simon.js
+++ js/simon.js
@@ -1,13 +1,12 @@
 // add playing and remove playing
 var play = function(btn) {
     btn.classList.add('playing');
-    
+    console.log(btn);
 }
 
 
 // add click event listeners
 const gameButtons = document.querySelectorAll('.game-button');
 gameButtons.forEach(button => button.addEventListener('click', function() {
-    console.log(this);
     play(this);
 }))
