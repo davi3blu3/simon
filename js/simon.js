af4653781fa635b0557d25b0fd9578f8a5fbb94b
Dave Harned <dharned@gmail.com>
2016-12-21 21:33:06 -0600

    create runSequence func

--- js/simon.js
+++ js/simon.js
@@ -1,4 +1,17 @@
-// add playing and remove playing
+var order = ['red', 'red', 'green', 'yellow', 'red', 'blue', 'blue', 'green', 'red', 'yellow', 'yellow'];
+var iteration = 0;
+var runSequence = function() {
+    setTimeout(function() {
+        play(document.querySelector('#' + order[iteration]));
+        iteration++;
+        if (iteration < order.length) {
+            runSequence();
+        }
+    }, 800)
+
+}
+
+// button sound and color change
 var play = function(btn) {
     // change color
     btn.classList.add('playing');
@@ -9,12 +22,15 @@ var play = function(btn) {
     setTimeout(function() {
         btn.classList.remove('playing');
     }, 500);
-    
 }
-
 
 // add click event listeners
 const gameButtons = document.querySelectorAll('.game-button');
 gameButtons.forEach(button => button.addEventListener('click', function() {
     play(this);
 }))
+
+const startButton = document.querySelector('#start');
+startButton.addEventListener('click', function() {
+    runSequence();
+})
