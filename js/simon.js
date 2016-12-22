119938e57003f5c63b7d425dbab8fddfec64f2fe
Dave Harned <dharned@gmail.com>
2016-12-21 20:08:24 -0600

    add audio

--- js/simon.js
+++ js/simon.js
@@ -1,9 +1,15 @@
 // add playing and remove playing
 var play = function(btn) {
+    // change color
     btn.classList.add('playing');
+    // play tone
+    var tone = document.querySelector(`audio[data-key="${btn.id}"]`);
+    tone.play();
+    // revert color after delay
     setTimeout(function() {
         btn.classList.remove('playing');
     }, 500);
+    
 }
 
 
